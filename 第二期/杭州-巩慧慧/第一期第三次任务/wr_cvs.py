115836fedcea63740a6b79ab4864f5a4436ac623
gonghuihui55 <gonghuihui55@163.com>
2018-03-26 22:56:07 +0800

    add wr_csv.py

--- 第二期/杭州-巩慧慧/第一期第三次任务/wr_cvs.py
+++ 第二期/杭州-巩慧慧/第一期第三次任务/wr_cvs.py
@@ -24,3 +24,27 @@ if __name__ == "__main__":
                 print(data, "  ")
 
         f.close()
+
+    print("Python cvs文件字典写读操作示例")
+    # 写
+    with open('csv_dict_data.csv', 'w') as csvfile:
+        # 写csv头
+        fieldnames = ['first_name', 'last_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        # 写csv内容
+        writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+        writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+        writer.writerow({'first_name': 'Worderful', 'last_name': 'Spam'})
+
+        print("读取csv_dict_data.csv内容")
+        with open('csv_dict_data.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            # print(reader)
+            for row in reader:
+                print(row['first_name'], row['last_name'])
+
+            # 为啥读不到数据
+
+
